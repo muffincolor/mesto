222f1a71a57175a55b1174ab82874be6daafe57d
Dmitry <Kononenko>
2020-07-02 11:14:10 +0300

    Внесение правок ревьювера

--- scripts/script.js
+++ scripts/script.js
@@ -10,20 +10,40 @@ let profileActivities = document.querySelector('.profile__activities');
 
 let closePopupBtn = document.querySelector('.popup__close-btn');
 
+
 function togglePopup() {
-  popup.classList.toggle('popup__active');
+  if (!popup.classList.contains('popup_active')) {
+    newName.value = profileName.textContent;
+    newActivities.value = profileActivities.textContent;
+  }
+  popup.classList.toggle('popup_active');
 }
 
-editButton.addEventListener('click', function () {
-  togglePopup();
-});
 
-submit.addEventListener('click', function () {
-  profileName.textContent = newName.value;
-  profileActivities.textContent = newActivities.value;
+editButton.addEventListener('click', function () {
   togglePopup();
 });
 
 closePopupBtn.addEventListener('click', function () {
   togglePopup();
 });
+
+
+
+let formElement = document.querySelector('.popup__form');
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+
+  let newName = document.querySelector('.popup__input_type_name');
+  let newActivities = document.querySelector('.popup__input_type_activities');
+
+  let profileName = document.querySelector('.profile__name');
+  let profileActivities = document.querySelector('.profile__activities');
+
+
+  profileName.textContent = newName.value;
+  profileActivities.textContent = newActivities.value;
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
